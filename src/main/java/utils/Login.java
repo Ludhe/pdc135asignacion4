0f7e5126831a7e9a9f2738bb15c85552bb1fff10
dmmaga <dmmaga@dmmagaDeb>
2020-05-21 20:55:05 -0600

    Cambio en la ip para conectarse

--- src/main/java/utils/Login.java
+++ src/main/java/utils/Login.java
@@ -59,7 +59,7 @@ public class Login {
         Dn dn;
         final String USER;
         final String DOMAIN = "tipicos";
-        connectionConfig.setLdapHost("ldap://25.16.250.146");
+        connectionConfig.setLdapHost("25.16.250.146");
         connectionConfig.setLdapPort(390);
         USER = "cn=" + usuario.getUid() + ",dc=" + DOMAIN + ",dc=occ,dc=ues,dc=edu,dc=sv";
         try {
